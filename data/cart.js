c308a2ed6353580e4bd1320efffa2d18cd16c7d5
Zavith Ibrahim <zavihibrahim4@gmail.com>
2026-08-07 18:24:35 +0530

    Initial commit

---
+++ data/cart.js
@@ -0,0 +1,97 @@
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+// JSON.parse will convert the string back to an array
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: "2",
+    },
+  ];
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  // to store the cart in the local storage to stop resetting after refresh
+}
+
+export function addToCart(productId) {
+  const quantity = document.querySelector(
+    `.js-product-quantity-${productId}`,
+  ).value;
+
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    // an object is a truthy value so if present it will return true
+    matchingItem.quantity += Number(quantity);
+  } else {
+    cart.push({
+      productId, // productId: productId
+      quantity: Number(quantity), // shorthand
+      deliveryOptionId: "1",
+    });
+  }
+
+  saveToLocalStorage();
+
+  // to update the cart
+}
+
+export function removeFromCart(productId) {
+  const newCartArray = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCartArray.push(cartItem);
+    }
+  });
+
+  cart = newCartArray; // replace the removed item cart
+
+  saveToLocalStorage();
+
+  // to update the cart
+  document.querySelector(".js-return-to-home-link").innerHTML =
+    `${cartQuantityUpdater()} items`;
+}
+
+export function cartQuantityUpdater() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      cartItem.quantity = newQuantity;
+
+      document.querySelector(`.js-quantity-label-${productId}`).innerHTML =
+        newQuantity;
+
+      document.querySelector(".js-return-to-home-link").innerHTML =
+        `${cartQuantityUpdater()} items`;
+    }
+  });
+
+  saveToLocalStorage();
+}
---
+++ js/amazon.js
@@ -0,0 +1,90 @@
+import { cart, addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+import { cartQuantityUpdater } from "../data/cart.js";
+let productsHTML = "";
+
+products.forEach((product) => {
+  productsHTML += /*template*/ `
+    <div class="product-container ">
+      <div class="product-image-container">
+        <img
+          class="product-image"
+          src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img
+          class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
+
+      <div class="product-quantity-container js-product-quantity-container">
+        <select class="js-product-quantity-${product.id}">
+          <option selected value="1" >1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-add-to-cart-${product.id}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button
+        class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id="${product.id}"
+        >
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
+
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+let timerId;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    clearTimeout(timerId);
+    const { productId } = button.dataset;
+    //this means const productId = button.dataset.productId;
+    const quantity = document.querySelector(
+      `.js-product-quantity-${productId}`,
+    ).value;
+    //to display add to cart
+
+    let addedMessage = document.querySelector(`.js-add-to-cart-${productId}`);
+    addedMessage.classList.add("display-add-to-cart");
+    timerId = setTimeout(() => {
+      addedMessage.classList.remove("display-add-to-cart");
+    }, 2000);
+    addToCart(productId);
+    document.querySelector(".js-cart-quantity").innerHTML =
+      cartQuantityUpdater();
+  });
+});
+
+document.querySelector(".js-cart-quantity").innerHTML = cartQuantityUpdater();
