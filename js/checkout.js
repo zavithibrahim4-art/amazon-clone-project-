520cec913590b2a0aeedcdf516ef487e1e1b7869
Zavith Ibrahim <zavihibrahim4@gmail.com>
2026-08-18 22:37:24 +0530

    created the cart using class

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,7 @@
 import checkoutHeader from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../data/cart-oop.js"
+import "../data/cart-class.js"
 renderOrderSummary();
 renderPaymentSummary();
 checkoutHeader();
