97af3d7acba5137dba4ea54f58a42816bd85190f
Zavith Ibrahim <zavihibrahim4@gmail.com>
2026-08-22 22:54:24 +0530

    converted the callbacks in the checkout.js to promises

--- js/checkout.js
+++ js/checkout.js
@@ -3,8 +3,68 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
+/* a promise is a class :
+  it takes a function with a param resolve
+  the resolve is a function which can be called anywhere inside the arrow function
+  the resolve will fulfills  the promise and move to the next step
+  but the resolve doesn't end the function like a return
+  the next step has to be declared with a .then(function) method
+  
+  
+  const promise1 = new Promise((resolve) => {
+  resolve("Promise 1");
+  //here the resolve takes a parameter and then the .then() gets the value as a parameter
+});
+
+const promise2 = new Promise((resolve) => {
+  resolve("Promise 2");
+});
+
+promise1.then((result) => {
+  console.log(result);
+});
+
+promise2.then((result) => {
+  console.log(result);
+}); in the above example each promise will wait for an another promise  it will take more time
+to rectify this issue we use promise.all()
+const promise1 = new Promise((resolve) => {
+  resolve("Promise 1");
+});
+
+const promise2 = new Promise((resolve) => {
+  resolve("Promise 2");
+});
+
+Promise.all([promise1, promise2])
+  .then((results) => {
+    console.log(results);
+  });*/
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  checkoutHeader();
+});
+
+/*
+new Promise((resolve) => {
+  function hi() {
+    console.log("hi");
+    resolve();
+    console.log("bye");
+  }
+  hi();
+}).then(() => {
+  console.log("finished");
+});
+/*
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
   checkoutHeader();
 });
+*/
